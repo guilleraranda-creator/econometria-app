705460d0a808b67ef48ea4c768b23d1b485d56c6
guilleraranda-creator <guilleraranda@gmail.com>
2025-12-05 18:53:34 +0100

    Create app.js

---
+++ docs/app.js
@@ -0,0 +1,17 @@
+function enviarNumero(numero) {
+    fetch("https://econometria-app.onrender.com/square", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ number: numero })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Resultado recibido del backend:", data);
+        alert("El resultado es: " + data.result);
+    })
+    .catch(error => {
+        console.error("Error al conectar con backend:", error);
+    });
+}
